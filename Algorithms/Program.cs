1f150c54b74f22b1f2519d08ed4582c452ee7c94
sufasah <sufasah@gmail.com>
2024-04-23 21:03:43 +0300

    feat: add algorithms project
    add heapsort, quicksort
    add djikstra

---
+++ Algorithms/Program.cs
@@ -0,0 +1,15 @@
+﻿using Algorithms;
+
+static void RunThreads(params ThreadStart[] operations)
+{
+    var threads = operations.Select(operation => new Thread(operation)).ToArray();
+    foreach (var thread in threads)
+        thread.Start();
+    foreach (var thread in threads)
+        thread.Join();
+}
+
+RunThreads(
+    () => Sorting.Test(Sorting.QuickSort),
+    () => Sorting.Test(Sorting.HeapSort),
+    () => Graph.Test());
